796db2b8b71db797d4b79a3981048f23b47fcd51
Charfeddine Mosrati <charfeddine.mosrati@polytechnicien.tn>
2025-08-15 00:13:04 +0100

    react skeleton

---
+++ client/src/App.tsx
@@ -0,0 +1,27 @@
+import { List, ListItem, ListItemText, Typography } from "@mui/material";
+import axios from "axios";
+import { useEffect, useState } from "react"
+
+function App() {
+  const [activities,setActivities] = useState<Activity[]>([]);
+  
+  useEffect(()=>{
+    axios.get<Activity[]>('http://localhost:5001/api/activities').then(response => setActivities(response.data))
+    // fetch('http://localhost:5001/api/activities').then(Response=>Response.json()).then(data => setActivities(data))
+
+  },[])
+  return (
+    <>
+      <Typography variant='h3' > Reactivities </Typography>
+      <List>
+       {activities.map((activity)=>(
+          <ListItem key={activity.id} >
+            <ListItemText>{activity.title}</ListItemText>
+          </ListItem>
+       ))}
+      </List>
+    </>
+  )
+}
+
+export default App
